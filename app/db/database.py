23df685ea537d84f0eccc3f08faaa3d4b8fa07f7
TheDanielV <126925561+TheDanielV@users.noreply.github.com>
2024-08-04 20:28:15 -0500

    Database connection only local

--- app/db/database.py
+++ app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:SecretPassword@vehiclesql--2iandx0.orangecliff-243aedf8.australiaeast.azurecontainerapps.io:3306/vehicle"
+DATABASE_URL = "mysql+pymysql://root:SecretPassword@vehiclesql:3306/vehicle"
 
 # Crear el motor de la base de datos
 engine = create_engine(DATABASE_URL, echo=True)
